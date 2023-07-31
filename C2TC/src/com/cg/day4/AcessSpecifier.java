bcb2f412104c1915c920bbedb5b8af9dbdf42350
prave <prave@pravee>
2023-07-31 16:07:36 +0530

    first

---
+++ C2TC/src/com/cg/day4/AcessSpecifier.java
@@ -0,0 +1,21 @@
+package com.cg.day4;
+
+class Private{  
+private int data=40;  
+private void display(){
+	System.out.println("Hello java");
+	}  
+
+public class AcessSpecifier {
+
+	public static void main(String[] args) {
+		Private p=new Private();  
+		
+	    
+		  // Private.display();//Compile Time Error 
+
+
+	}
+
+}
+}
